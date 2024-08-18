94881e3c59f77416167149a25b07d14a060137ac
chittaro <chittaro@umich.edu>
2024-08-18 09:57:07 -0400

    actual requirements commit

--- ui/interface.py
+++ ui/interface.py
@@ -1,8 +1,13 @@
-from dash import Dash
+from dash import Dash, html
 import pandas as pd
 
 
-df = pd.DataFrame()
-print(df)
+df = pd.DataFrame({"Monday": {"Easy": 10, "Medium": 5, "Hard": 2}, "Tuesday": {"Easy": 14, "Medium": 6, "Hard": 2}})
 
 app = Dash(__name__, external_stylesheets=['style.css'])
+app.title('KTP LeetCode Tracker')
+
+app.layout = [html.Div(children='Hello World')]
+
+if __name__ == '__main__':
+    app.run(debug=True)
